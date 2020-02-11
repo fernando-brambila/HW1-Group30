75611010c3202b75f2e2cdf5e6577d69e82daf68
fernando-brambila <brambif@wwu.edu>
2020-02-10 17:05:50 -0800

    First commit

---
+++ src/Constants.java
@@ -0,0 +1,12 @@
+public class Constants {
+
+    // warrior, thief classes
+
+    public static int WARRIOR_HEALTH = 100;
+    public static int WARRIOR_DAMAGE = 15;
+    public static int THIEF_HEALTH = 70;
+    public static int THIEF_DAMAGE = 10;
+    public static double THIEF_GP_MODIFIER = 1.2;
+
+
+}
