7d6eaac7c59deece653de06756824e6c07a8574f
nick-johnson <59623912+johnson-nick@users.noreply.github.com>
2020-02-18 21:21:05 -0800

    Update Monster.java

--- src/Monster.java
+++ src/Monster.java
@@ -1,14 +1,31 @@
-
+import java.util.Random;
 public class Monster {
+
     private int health;
     private int damage;
     private String monsterType;
+    Random myrand = new Random();
+
+    Monster(int h, int d, String type){
+        this.health = h;
+        this.damage = d;
+        this.monsterType = type;
+    }
+
+
+
+
 
     public void attack(Player target) {
-
+        target.onHit(myrand.nextInt(damage));
     }
+
+
 
     public void onHit(int damage) {
 
+
+
     }
+
 }
